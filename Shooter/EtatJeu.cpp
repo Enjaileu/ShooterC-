d4b80b27a9f69cf95b34131130e9ac49f9261556
Angele SIONNEAU <a.sionneau@hotmail.fr>
2022-09-28 20:37:01 +0200

    shooter-07

--- Shooter/EtatJeu.cpp
+++ Shooter/EtatJeu.cpp
@@ -8,6 +8,13 @@ void EtatJeu::Update(float dt) {
 	for (Tir* tir : tirs) {
 		tir->Update(dt);
 	}
+	for (int i = tirs.size() - 1; i >= 0; --i) {
+		if (tirs[i]->x > Constants::SCREEN_WIDTH) {
+			tirs[i]->Unload();
+			tirs.erase(begin(tirs) + i);
+			TraceLog(LOG_INFO, "tir suppr");
+		}
+	}
 
 	if (IsKeyPressed(KEY_SPACE)) {
 		tirs.push_back(vaisseau.Tirer());
